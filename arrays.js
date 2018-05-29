f47f84c6795cb7a4d3e846cafd2d6f4f048d06d2
kapr.natasha@gmail.com <kapr.natasha@gmail.com>
2018-05-29 15:40:01 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,22 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(arr, addElement) {
+  arr = [addElement, ...arr]
+  return arr
+
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, addElement) {
+  arr.unshift(addElement)
+  return arr
+}
+
+function addElementToEndOfArray(arr, addElement) {
+  arr = [...arr, addElement]
+  return arr
+}
+
+function destructivelyAddElementToEndOfArray(arr, addElement) {
+  arr.push(addElement)
+  return arr
+}
